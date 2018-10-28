598c299adfa579dc8055319ae51d7d9d5881657c
DESKTOP-0RTEV8U\lebom <lebomfin@gmail.com>
2018-10-27 23:38:57 -0300

    First Commit Calculadora Program Unit Tests

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Text;
+
+
+
+namespace Calculadora
+
+{
+    public class CalculadoraSimples
+    {
+
+        public double Soma(double num1, double num2)
+        {
+            double soma;
+            soma = num1 + num2;
+            return soma;
+        }
+
+        public double Subtracao(double num1, double num2)
+        {
+            double subtracao;
+            subtracao = num1 - num2;
+            return subtracao;
+        }
+
+        public double Divisao(double num1, double num2)
+        {
+            double divisao;
+            divisao = num1 / num2;
+            return divisao;
+        }
+
+        public double Multiplicacao(double num1, double num2)
+        {
+            double multiplicacao;
+            multiplicacao = num1 * num2;
+            return multiplicacao;
+        }
+
+
+
+
+    }
+
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+            double num1, num2;
+
+            int resul = 0;
+
+
+            Console.WriteLine("Digite o primeiro numero");
+
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero");
+
+            num2 = double.Parse(Console.ReadLine());
+
+
+
+            Console.Clear();
+
+
+
+            while (resul != 5)
+
+            {
+                Console.WriteLine("SELECIONE QUAL OPERAÇÃO DESEJA EFETUAR:");
+
+                Console.WriteLine("1. Somar");
+
+                Console.WriteLine("2. Subtrair");
+
+                Console.WriteLine("3. Dividir");
+
+                Console.WriteLine("4. Multiplicar");
+
+                Console.WriteLine("5. Sair");
+
+                resul = int.Parse(Console.ReadLine());
+
+                if (resul == 1)
+
+                {
+
+                    Console.WriteLine("Soma = {0}", calculadora.Soma(num1, num2));
+
+                }
+
+                if (resul == 2)
+
+                {
+
+                    Console.WriteLine("Subtração = {0}", calculadora.Subtracao(num1, num2));
+
+                }
+
+                if (resul == 3)
+
+                {
+
+                    Console.WriteLine("Divisão = {0}", calculadora.Divisao(num1, num2));
+
+                }
+
+                if (resul == 4)
+
+                {
+
+                    Console.WriteLine("Multiplicação = {0}", calculadora.Multiplicacao(num1, num2));
+
+                }
+
+                Console.ReadLine();
+
+                Console.Clear();
+
+            }
+
+        }
+
+    }
+
+}
---
+++ Calculadora/UnitTestCalculadora/CalculadoraTestes.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Calculadora;
+using NUnit.Framework;
+
+namespace UnitTestCalculadora
+{
+    [TestFixture]
+
+    public class CalculadoraTestes
+    {
+
+        [Test]
+        public void ValidaSomaCorreta()
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+
+            var result = calculadora.Soma(5, 3);
+            Assert.That(result, Is.EqualTo(8));
+        }
+
+        [Test]
+        public void ValidaSomaIncorreta()
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+
+            var result = calculadora.Soma(9, 21);
+            Assert.That(result, Is.Not.EqualTo(31));
+        }
+
+        [Test]
+        public void ValidaSubtracaoCorreta()
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+
+            var result = calculadora.Subtracao(10, 4);
+            NUnit.Framework.Assert.That(result, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void ValidaSubtracaoCorretaNegativa()
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+
+            var result = calculadora.Subtracao(5, 9);
+            NUnit.Framework.Assert.That(result, Is.EqualTo(-4));
+        }
+
+        [Test]
+        public void ValidaSubtracaoIncorreta()
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+
+            var result = calculadora.Subtracao(76, 5);
+            NUnit.Framework.Assert.That(result, Is.Not.EqualTo(70));
+        }
+
+        [Test]
+        public void ValidaMultiplicacaoCorreta()
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+
+            var result = calculadora.Multiplicacao(6, 7);
+            NUnit.Framework.Assert.That(result, Is.EqualTo(42));
+        }
+
+        [Test]
+        public void ValidaMultiplicacaoIncorreta()
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+
+            var result = calculadora.Multiplicacao(8, 7);
+            NUnit.Framework.Assert.That(result, Is.Not.EqualTo(55));
+        }
+
+        [Test]
+        public void ValidaDivisaoCorretaInteira()
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+
+            var result = calculadora.Divisao(48, 6);
+            NUnit.Framework.Assert.That(result, Is.EqualTo(8));
+        }
+
+        [Test]
+        public void ValidaDivisaoCorretaDecimal()
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+
+            var result = calculadora.Divisao(57, 8);
+            NUnit.Framework.Assert.That(result, Is.EqualTo(7.125));
+        }
+
+        [Test]
+        public void ValidaDivisaoIncorreta()
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+
+            var result = calculadora.Divisao(99, 33);
+            NUnit.Framework.Assert.That(result, Is.Not.EqualTo(11));
+        }
+    }
+} 
